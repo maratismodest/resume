04ca0010c7c301b8a5e869fd14db7293b7aa5587
Marat Faizerakhmanov <maratismodest@gmail.com>
2023-05-19 17:42:56 +0300

    Update route.js

--- app/api/screenshot/route.js
+++ app/api/screenshot/route.js
@@ -10,11 +10,7 @@ export async function GET(req, res) {
         await page.goto('https://marat-faizerakhmanov.vercel.app', {waitUntil: 'networkidle2'});
         const pdfBuffer = await page.pdf({format: 'A4'});
 
-        await browser.close();
-
-        return new Response(pdfBuffer, {
-            status: 200,
-        });
+        return new Response(pdfBuffer);
     } catch (error) {
         res.status(500).json({error: error.message});
     } finally {
